d673aebd01dc8acb0de0797756f13002d1a54ffc
KsArT-IT <ksart.it@gmail.com>
2024-06-24 14:35:04 +0300

    refactor: change application

---
+++ WorkoutTracking/application/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  WorkoutTracking
+//
+//  Created by KsArT on 17.06.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
